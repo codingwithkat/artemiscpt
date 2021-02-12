7a33133a8581075421fc212d64b0ed50bda01fa2
Katerina Scoullos <katpapaloukas@gmail.com>
2021-02-12 12:37:56 -0500

    logo

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -4,6 +4,7 @@ class Welcome extends React.Component {
   render() {
     return (
       <div>
+        {/* logo */}
         <img
           className="logo"
           src="https://img1.wsimg.com/isteam/ip/54c7c7a5-e068-4db7-9440-392f9a83b224/Final.png/:/cr=t:0%25,l:0%25,w:100%25,h:100%25"
