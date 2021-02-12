78fcac43727894ed9d9e614affd8b773043a9b33
Katerina Scoullos <katpapaloukas@gmail.com>
2021-02-12 12:44:25 -0500

    db

--- server/db/db.js
+++ server/db/db.js
@@ -2,15 +2,12 @@ const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-let dbConfig = {}
 
-// SSL can only be used in production environment
-// run the following command 'echo $NODE_ENV', it should return 'production'
-// if we're in production, use SSL
-if (process.env.NODE_ENV === 'production') {
-  dbConfig = {
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
     logging: false,
-    operatorsAliases: 0,
+    operatorsAliases: false,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
@@ -20,40 +17,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     }
   }
-} else {
-  // otherwise run in development mode
-  dbConfig = {
-    logging: false,
-    operatorsAliases: 0,
-    dialect: 'postgres',
-    protocol: 'postgres'
-  }
-}
-
-const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  dbConfig
 )
-
-module.exports = db
-
-// const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
-
-// const db = new Sequelize(
-//   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-//   {
-//     logging: false,
-//     operatorsAliases: false,
-//     dialect: 'postgres',
-//     protocol: 'postgres'
-//     // dialectOptions: {
-//     //   ssl: {
-//     //     require: true,
-//     //     rejectUnauthorized: false
-//     //   }
-//     // }
-//   }
-// )
 module.exports = db
 
 //ssl true for deploy needed
